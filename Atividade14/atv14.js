7009a228524827f6d3efdbc1ad0c027c4644bf88
Guilherme U. Iwasaki <101954273+Pesagniya@users.noreply.github.com>
2024-05-06 12:07:38 -0300

    Add files via upload

---
+++ Atividade14/atv14.js
@@ -0,0 +1,7 @@
+document.getElementById("up").addEventListener('change', () => {
+    entrada.value= entrada.value.toUpperCase();
+});
+
+document.getElementById("low").addEventListener('change', () => {
+    entrada.value= entrada.value.toLowerCase();  
+});
